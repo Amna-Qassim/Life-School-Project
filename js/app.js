9e2f37e3e8b88994f88a3a21068907edfef22588
Amna Kassim <84710020+Amna-Qassim@users.noreply.github.com>
2021-10-30 14:51:49 +0300

    the last editation

--- js/app.js
+++ js/app.js
@@ -15,3 +15,24 @@ $('.popup .close').click(function(e) {
   e.preventDefault();
   $('.popup').fadeOut();
 });
+
+
+//Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  // Get values
+  var firstName = getInputVal('firstName');
+  var lastname = getInputVal('lasttName');
+  var inputEmail4 = getInputVal('inputEmail4');
+  var inputPassword4 = getInputVal('inputPassword4');
+  var inputCity = getInputVal('inputCity');
+
+}
+
+//Function to grt form values
+function getInputVal(id) {
+  return document.getElementsById(id).value;
+}
